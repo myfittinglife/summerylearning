d2f0f7e5f25a8317d33afb089b18918cec4a8fd1
ld <836354552@qq.com>
2018-10-18 09:01:15 +0800

    代码方式添加帧动画

--- app/src/main/java/com/example/administrator/summarylearning/animation/AnimationActivity.java
+++ app/src/main/java/com/example/administrator/summarylearning/animation/AnimationActivity.java
@@ -1,11 +1,13 @@
 package com.example.administrator.summarylearning.animation;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.administrator.summarylearning.R;
 
@@ -23,6 +25,8 @@ public class AnimationActivity extends AppCompatActivity {
     Button btn_start_frame_animation;
     @BindView(R.id.btn_stop_frame_animation)
     Button btn_stop_frame_animation;
+    @BindView(R.id.btn_add_frame_animation)
+    Button btn_add_frame_animation;
     @BindView(R.id.iv_frame_animation)
     ImageView iv_frame_animation;
 
@@ -34,18 +38,39 @@ public class AnimationActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
     }
-    @OnClick({R.id.btn_start_frame_animation,R.id.btn_stop_frame_animation})
+    @OnClick({R.id.btn_start_frame_animation,R.id.btn_stop_frame_animation,R.id.btn_add_frame_animation})
     public void onClick(View view){
         switch (view.getId()){
             case R.id.btn_start_frame_animation:                                                                  //开启帧动画
                 iv_frame_animation.setBackgroundResource(R.drawable.frame_animation_list);
-                AnimationDrawable animationDrawable= (AnimationDrawable) iv_frame_animation.getBackground();
+                AnimationDrawable animationDrawable= (AnimationDrawable) iv_frame_animation.getBackground();      //通过animation方式添加帧动画
                 animationDrawable.start();
                 break;
             case R.id.btn_stop_frame_animation:                                                                   //关闭帧动画
                 AnimationDrawable stopAnimationDrawable= (AnimationDrawable) iv_frame_animation.getBackground();
-                stopAnimationDrawable.stop();
+                if(stopAnimationDrawable!=null){
+                    stopAnimationDrawable.stop();
+                }else {
+                    Toast.makeText(getApplicationContext(),"没有帧动画",Toast.LENGTH_SHORT).show();
+                }
                 break;
+            case R.id.btn_add_frame_animation:
+                addFrame();
+                break;
+
         }
     }
+    //通过代码方式添加帧动画
+    public void addFrame(){
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        int[] mipmaps = new int[]{R.drawable.run1, R.drawable.run2, R.drawable.run3, R.drawable.run4, R.drawable.run5, R.drawable.run6, R.drawable.run7};
+        for(int i = 0;i<mipmaps.length;i++){
+            Drawable drawable = getResources().getDrawable(mipmaps[i]);
+            animationDrawable.addFrame(drawable,200);
+        }
+        animationDrawable.setOneShot(false);
+        iv_frame_animation.setBackgroundDrawable(animationDrawable);
+        animationDrawable.start();
+    }
+
 }
